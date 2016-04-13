0a0c41c1ed360df6fd8cdecf1e4ac2419ece9605
belzen <njepsen89@gmail.com>
2016-04-12 21:59:01 -0700

    Post-processing now uses SV_VertexID semantic to generate the geometry in the vertex shader instead of using vertex buffers.

--- data/shaders/v_screen.hlsl
+++ data/shaders/v_screen.hlsl
@@ -1,19 +1,18 @@
 #include "v_output.hlsli"
 #include "v_common.hlsli"
 
-struct VertexInput
-{
-	float2 position : POSITION;
-	float2 texcoords : TEXCOORD0;
-};
 
-VsOutputSprite main(VertexInput input)
+VsOutputSprite main(uint id : SV_VertexID)
 {
 	VsOutputSprite output = (VsOutputSprite)0;
 
-	// Geo is already in screenspace, just pass through.
-	output.position = float4(input.position.xy, 0.f, 1.f);
-	output.texcoords = input.texcoords;
+	output.position.x = (id / 2) * 4.f - 1.f;
+	output.position.y = (id % 2) * 4.f - 1.f;
+	output.position.z = 0.f;
+	output.position.w = 1.f;
+
+	output.texcoords.x = (id / 2) * 2.f;
+	output.texcoords.y = 1.f - (id % 2) * 2.f;
 	
 	return output;
 }
